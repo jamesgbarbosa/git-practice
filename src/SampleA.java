51b91a5d153bc7e424f82ea92bbe3b5df4a668f5
jambarbo <james.barbosa@nokia.com>
2017-12-18 21:59:13 +0800

    Master change

--- src/SampleA.java
+++ src/SampleA.java
@@ -2,5 +2,6 @@ public class SampleA {
     public static void main(String[] args) {
         System.out.println("Print A. Changes from Branch A");
         System.out.println("Change from BranchA");
+        System.out.println("Print A. Master Change");
     }
 }
