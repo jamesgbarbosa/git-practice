4637d48df90d7e9cc9f840ee45211145a1b30710
jambarbo <james.barbosa@nokia.com>
2017-12-18 22:14:40 +0800

    Reverted MASTER

--- src/SampleA.java
+++ src/SampleA.java
@@ -1,13 +1,5 @@
 public class SampleA {
     public static void main(String[] args) {
-        System.out.println("Print A. Changes from Branch A");
-        System.out.println("Change from BranchA");
-        System.out.println("Print A. Master Change");
-
-        System.out.println("Print A. new commit from Branch A");
-        System.out.println("Print B.  new commit from Branch A");
-        System.out.println("Print C.  new commit from Branch A");
-
-
+        System.out.println("MASTER");
     }
 }
